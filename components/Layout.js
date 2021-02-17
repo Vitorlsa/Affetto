a41c7f8e86b76c135de8fc8d168ee3ceca7cfc25
Vitorlsa <47422522+Vitorlsa@users.noreply.github.com>
2021-02-16 21:43:36 -0300

    Day 16/02
    Added lines for mobile menu(navbar) but it didn't work. Try more later.

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,14 @@ const Layout = ({children}) => {
     return (
         <div>
             <Head>
+                //Google Icon Font
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                //Compiled and minified CSS
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+                //Materialize js
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+                //jQuery
+                <script src="https://code.jquery.com/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk=" crossorigin="anonymous"></script>
             </Head>
             <Navbar />
         {children}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 
@@ -13,20 +14,37 @@ const Navbar = () => {
             return " "
         }
     }
-    
+
+/*    useEffect(() =>{
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelector('.sidenav');
+            var instances = M.Sidenav.init(elems, options);
+          });
+    })
+*/
+
     return (
-        <nav>
-            <div className="nav-wrapper #ffcdd2 red lighten-4">
-                <Link href="/"><a href="#" className="brand-logo">Logo</a></Link>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li className={isActive("/")}><Link href="/"><a>Home</a></Link></li>
-                    <li className={isActive("/assessorias")}><Link href="/assessorias"><a>Tipos de Assessoria</a></Link></li>
-                    <li className={isActive("/galeria")}><Link href="/galeria"><a>Galeria</a></Link></li>
-                    <li className={isActive("/depoimentos")}><Link href="/depoimentos"><a>Depoimentos</a></Link></li>
-                    <li className={isActive("/faleconosco")}><Link href="/faleconosco"><a>Fale conosco</a></Link></li>
-                </ul>
-            </div>
-        </nav>
+        <div>
+            <nav>
+                <div className="nav-wrapper #ffcdd2 red lighten-4">
+                    <Link href="/"><a href="#" className="brand-logo">Logo</a></Link>
+                    {/*<a href="#" data-target="mobile-demo" className="sidenav-trigger"><i class="material-icons">menu</i></a>*/}
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li className={isActive("/")}><Link href="/"><a>Home</a></Link></li>
+                        <li className={isActive("/assessorias")}><Link href="/assessorias"><a>Tipos de Assessoria</a></Link></li>
+                        <li className={isActive("/galeria")}><Link href="/galeria"><a>Galeria</a></Link></li>
+                        <li className={isActive("/depoimentos")}><Link href="/depoimentos"><a>Depoimentos</a></Link></li>
+                        <li className={isActive("/faleconosco")}><Link href="/faleconosco"><a>Fale conosco</a></Link></li>
+                    </ul>
+                </div>
+            </nav>
+        {/* <ul className="sidenav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>*/}
+        </div>
     );
 }
 
